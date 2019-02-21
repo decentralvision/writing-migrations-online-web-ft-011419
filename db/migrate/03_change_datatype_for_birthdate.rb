1da5c35674e5f90a1dca5360f4f0e4f6dc3fbe06
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-21 14:37:55 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   change
     change_column :students, :birthdate, :datetime
   end
